f658c20f52d808081786ea17b49d0b16df12292d
Gabe <wei.g.chu@gmail.com>
2020-03-12 12:06:21 +0800

    renamed folder

--- tokenizers/detokenizer.py
+++
@@ -1,5 +0,0 @@
-from typing import List
-
-
-def space_join_detokenzier(tokens: List[str]) -> str:
-    return " ".join(tokens)
--- tokenizers/token.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Token:
-    text: str
-    start: int
-    end: int
