6a09a2c05aee61bd1a3dd5eacae0e02d8f545d3a
Gabe <wei.g.chu@gmail.com>
2020-03-13 14:48:00 +0800

    black format

--- evaluation/metrics_test.py
+++ evaluation/metrics_test.py
@@ -3,20 +3,20 @@ from .metrics import compute_f_beta
 
 
 def test_compute_f_beta():
-    actual = compute_f_beta(0., 0.)
+    actual = compute_f_beta(0.0, 0.0)
     assert np.isnan(actual)
 
-    actual = compute_f_beta(np.nan, 1.)
+    actual = compute_f_beta(np.nan, 1.0)
     assert np.isnan(actual)
 
-    actual = compute_f_beta(1., np.nan)
+    actual = compute_f_beta(1.0, np.nan)
     assert np.isnan(actual)
 
-    actual = compute_f_beta(1., 1.)
-    assert actual == 1.
+    actual = compute_f_beta(1.0, 1.0)
+    assert actual == 1.0
 
-    actual = compute_f_beta(0.3, 0.5, beta=1.)
+    actual = compute_f_beta(0.3, 0.5, beta=1.0)
     assert np.isclose(actual, 0.3749999999)
 
-    actual = compute_f_beta(0.3, 0.5, beta=2.)
+    actual = compute_f_beta(0.3, 0.5, beta=2.0)
     assert np.isclose(actual, 0.44117647058)
