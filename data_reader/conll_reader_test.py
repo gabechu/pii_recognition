439a5f13339debe95ffefc28b071b95cceade7a1
Gabe <wei.g.chu@gmail.com>
2020-03-16 16:12:49 +0800

    add test for get_conll_eval_data

--- data_reader/conll_reader_test.py
+++ data_reader/conll_reader_test.py
@@ -1,5 +1,9 @@
-from .conll_reader import sent2labels, sent2tokens
+from mock import Mock, patch
 from pytest import fixture
+
+from tokeniser.detokeniser import space_join_detokensier
+
+from .conll_reader import get_conll_eval_data, sent2labels, sent2tokens
 
 
 @fixture
@@ -10,6 +14,29 @@ def sent():
         ("JAPAN", "NNP", "I-LOC"),
         ("GET", "VB", "O"),
     ]
+
+
+def mock_ConllCorpusReader():
+    mock = Mock()
+    mock.return_value.iob_sents.return_value = [
+        [
+            ("SOCCER", "NN", "O"),
+            ("-", ":", "O"),
+            ("JAPAN", "NNP", "I-LOC"),
+            ("GET", "VB", "O"),
+        ],
+        [("Nadim", "NNP", "I-PER"), ("Ladki", "NNP", "I-PER")],
+    ]
+    return mock
+
+
+@patch("data_reader.conll_reader.ConllCorpusReader", new=mock_ConllCorpusReader())
+def test_get_conll_eval_data():
+    sents, labels = get_conll_eval_data(
+        file_path="fake_path", detokenizer=space_join_detokensier
+    )
+    assert sents == ["SOCCER - JAPAN GET", "Nadim Ladki"]
+    assert labels == [["O", "O", "I-LOC", "O"], ["I-PER", "I-PER"]]
 
 
 def test_sent2tokens(sent):
