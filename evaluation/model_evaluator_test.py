5307820f802ca0c8b4f905b6048548bf8032558a
Gabe <wei.g.chu@gmail.com>
2020-03-16 13:20:43 +0800

    test evalutate_all

--- evaluation/model_evaluator_test.py
+++ evaluation/model_evaluator_test.py
@@ -147,8 +147,30 @@ def test_evaluate_sample(text, mock_recogniser, mock_tokeniser):
     assert mistakes == SampleError(token_errors=[], full_text=text, failed=False)
 
 
-def test_evaulate_all():
-    ...
+def test_evaulate_all(text, mock_recogniser, mock_tokeniser):
+    evaluator = ModelEvaluator(
+        recogniser=mock_recogniser,
+        target_entities=["PER", "LOC"],
+        tokeniser=mock_tokeniser,
+    )
+    counters, mistakes = evaluator.evaulate_all(
+        texts=[text] * 2, annotations=[["O", "O", "PER", "O", "LOC", "O"]] * 2
+    )
+
+    assert (
+        counters
+        == [
+            Counter(
+                {
+                    EvalLabel("O", "O"): 4,
+                    EvalLabel("LOC", "LOC"): 1,
+                    EvalLabel("PER", "PER"): 1,
+                }
+            )
+        ]
+        * 2
+    )
+    assert mistakes == [SampleError(token_errors=[], full_text=text, failed=False)] * 2
 
 
 def test_calculate_score():
