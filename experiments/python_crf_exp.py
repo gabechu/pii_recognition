e11af516aaa0a92974812402dfaac24bc7900c84
Gabe <wei.g.chu@gmail.com>
2020-03-19 12:35:08 +0800

    add create experiment for mlflow

--- experiments/python_crf_exp.py
+++ experiments/python_crf_exp.py
@@ -1,9 +1,24 @@
-from evaluation.model_evaluator import ModelEvaluator
+import os
+from typing import List
+
+import mlflow
+
 from data_reader.conll_reader import get_conll_eval_data
+from evaluation.model_evaluator import ModelEvaluator
 from recognisers.crf_recogniser import CrfRecogniser
 from tokeniser.detokeniser import space_join_detokensier
 from tokeniser.tokeniser import nltk_word_tokenizer
-import mlflow
+from utils import write_iterable_to_text
+
+from .manage_experiments import CRF_EXP
+
+RUN_NAME = "word2feature"
+
+
+def write_plain_text(content: List, file_path: str):
+    with open(file_path, "w") as f:
+        for elem in content:
+            f.write(str(elem) + "\n")
 
 
 # init recogniser
@@ -30,7 +45,9 @@ X_test, y_test = get_conll_eval_data(
 )
 
 
-with mlflow.start_run(run_name="python_crf_suite"):
+# Log to mlflow
+mlflow.set_experiment(CRF_EXP)
+with mlflow.start_run(run_name=RUN_NAME):
     evaluator = ModelEvaluator(recogniser, ["I-PER"], nltk_word_tokenizer)
 
     counters, mistakes = evaluator.evaulate_all(X_test, y_test)
@@ -38,6 +55,11 @@ with mlflow.start_run(run_name="python_crf_suite"):
     mistakes = list(filter(lambda x: x.token_errors, mistakes))
     recall, precision, f1 = evaluator.calculate_score(counters, f_beta=1.0)
     _, _, f2 = evaluator.calculate_score(counters, f_beta=2.0)
+
+    write_iterable_to_text(mistakes, f"{RUN_NAME}.mis")
+    mlflow.log_artifact(f"{RUN_NAME}.mis")
+
+    os.remove(f"{RUN_NAME}.mis")
 
     mlflow.log_metric("PER_recall", recall["I-PER"])
     mlflow.log_metric("PER_precision", precision["I-PER"])
