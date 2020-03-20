3e03afceab92859b358450ab127320a44cbbbcba
Gabe <wei.g.chu@gmail.com>
2020-03-20 10:24:34 +0800

    use log_evalution func

--- experiments/python_crf_exp.py
+++ experiments/python_crf_exp.py
@@ -1,20 +1,14 @@
-import os
-
-import mlflow
-
 from data_reader.conll_reader import get_conll_eval_data
 from evaluation.model_evaluator import ModelEvaluator
 from recognisers.crf_recogniser import CrfRecogniser
 from tokeniser.detokeniser import space_join_detokensier
 from tokeniser.tokeniser import nltk_word_tokenizer
-from utils import write_iterable_to_text
 
 from .manage_experiments import CRF_EXP
+from .mlflow_log import log_evaluation_to_mlflow
 
 RUN_NAME = "word2feature"
 
-
-# init recogniser
 recogniser = CrfRecogniser(
     supported_entities=[
         "B-LOC",
@@ -30,31 +24,10 @@ recogniser = CrfRecogniser(
     model_path="exported_models/conll2003-en.crfsuite",
     tokenizer=nltk_word_tokenizer,
 )
+evaluator = ModelEvaluator(recogniser, ["I-PER"], nltk_word_tokenizer)
 
-
-# Prepare evalution data
 X_test, y_test = get_conll_eval_data(
     file_path="datasets/conll2003/eng.testb", detokenizer=space_join_detokensier
 )
 
-
-# Log to mlflow
-mlflow.set_experiment(CRF_EXP)
-with mlflow.start_run(run_name=RUN_NAME):
-    evaluator = ModelEvaluator(recogniser, ["I-PER"], nltk_word_tokenizer)
-
-    counters, mistakes = evaluator.evaulate_all(X_test, y_test)
-    # remove returns with no mistakes
-    mistakes = list(filter(lambda x: x.token_errors, mistakes))
-    recall, precision, f1 = evaluator.calculate_score(counters, f_beta=1.0)
-    _, _, f2 = evaluator.calculate_score(counters, f_beta=2.0)
-
-    write_iterable_to_text(mistakes, f"{RUN_NAME}.mis")
-    mlflow.log_artifact(f"{RUN_NAME}.mis")
-
-    os.remove(f"{RUN_NAME}.mis")
-
-    mlflow.log_metric("PER_recall", recall["I-PER"])
-    mlflow.log_metric("PER_precision", precision["I-PER"])
-    mlflow.log_metric("PER_f1", f1["I-PER"])
-    mlflow.log_metric("PER_f2", f2["I-PER"])
+log_evaluation_to_mlflow(CRF_EXP, RUN_NAME, recogniser, evaluator, X_test, y_test)
