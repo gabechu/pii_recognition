6d63b461e80a5c836862d5f57c94659567c192a0
Gabe <wei.g.chu@gmail.com>
2020-03-20 14:35:11 +0800

    add flair model

---
+++ recognisers/flair.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+from label.label_schema import SpanLabel
+
+from .entity_recogniser import EntityRecogniser
+
+
+class Flair(EntityRecogniser):
+    def __init__(
+        self, supported_entities: List[str], supported_languages: List[str],
+    ):
+        super().__init__(
+            supported_entities=supported_entities,
+            supported_languages=supported_languages,
+        )
+
+    def load_model(self):
+        return SequenceTagger.load("ner")
+
+    def analyse(self, text: str, entities: List[str]) -> List[SpanLabel]:
+        sentence = Sentence(text)
+        self._model.predict(sentence)
+
+        span_labels = []
+        for entity in sentence.get_spans("ner"):
+            span_labels.append(SpanLabel(entity.tag, entity.start_pos, entity.end_pos))
+
+        return span_labels
