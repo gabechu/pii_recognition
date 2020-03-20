f0e694f43958ea41436638ea64c13459d9b966b8
Gabe <wei.g.chu@gmail.com>
2020-03-20 11:38:02 +0800

    add treebank detokeniser

--- tokeniser/detokeniser.py
+++ tokeniser/detokeniser.py
@@ -1,5 +1,10 @@
 from typing import List
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 
 def space_join_detokensier(tokens: List[str]) -> str:
     return " ".join(tokens)
+
+
+def treebank_detokeniser(tokens: List[str]) -> str:
+    return TreebankWordDetokenizer().detokenize(tokens)
--- tokeniser/detokeniser_test.py
+++ tokeniser/detokeniser_test.py
@@ -1,7 +1,13 @@
-from .detokeniser import space_join_detokensier
+from .detokeniser import space_join_detokensier, treebank_detokeniser
 
 
 def test_space_join_detokeniser():
     tokens = ["Here", "is", "a", "test", "."]
     actual = space_join_detokensier(tokens)
     assert actual == "Here is a test ."
+
+
+def test_treebank_detokeniser():
+    tokens = ["Here", "is", "a", "test", "."]
+    actual = treebank_detokeniser(tokens)
+    assert actual == "Here is a test."
