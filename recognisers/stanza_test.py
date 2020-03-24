a822242ca6f07aca8159bb3ccba757216c404203
Gabe <wei.g.chu@gmail.com>
2020-03-24 10:33:19 +0800

    add test for stanza

---
+++ recognisers/stanza_test.py
@@ -0,0 +1,51 @@
+from typing import NamedTuple
+
+import pytest
+from mock import Mock, patch
+from pytest import fixture
+
+from label.label_schema import SpanLabel
+
+from .stanza import Stanza
+
+
+class MockStanzaSpan(NamedTuple):
+    type: str
+    start_char: int
+    end_char: int
+
+
+def mock_pipeline():
+    pipeline = Mock()
+    pipeline.return_value.return_value.entities = [
+        MockStanzaSpan("PERSON", 8, 11),
+        MockStanzaSpan("LOC", 17, 26),
+    ]
+    return pipeline
+
+
+@fixture
+def text():
+    return "This is Bob from Melbourne."
+
+
+@patch("recognisers.stanza.Pipeline", new=mock_pipeline())
+def test_stanza_analyse(text):
+    recogniser = Stanza(
+        supported_entities=["PERSON", "LOC", "ORG"],
+        supported_languages=["en"],
+        model_name="en",
+    )
+
+    actual = recogniser.analyse(text, entities=["PERSON"])
+    assert actual == [SpanLabel("PERSON", 8, 11)]
+
+    actual = recogniser.analyse(text, entities=["PERSON", "LOC"])
+    assert actual == [SpanLabel("PERSON", 8, 11), SpanLabel("LOC", 17, 26)]
+
+    with pytest.raises(AssertionError) as err:
+        recogniser.analyse(text, entities=["PERSON", "LOC", "TIME"])
+    assert (
+        str(err.value)
+        == "Only support ['PERSON', 'LOC', 'ORG'], but got ['PERSON', 'LOC', 'TIME']"
+    )
