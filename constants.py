bc5745696d5abb59d611c14121f11e3b34704ede
Gabe <wei.g.chu@gmail.com>
2020-03-25 16:24:38 +0800

    add inline comment

--- constants.py
+++ constants.py
@@ -3,4 +3,8 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
+# reduce label discrepancies in evaluation metrics when
+# running on distinct datasets, e.g., CONLL is using "I-PER" to
+# reference person but in WNUT the label is "I-person"
 LABEL_COMPLIANCE = {"I-PER": "PERSON", "I-person": "PERSON"}
