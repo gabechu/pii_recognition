c84f3ef324a0ef497e02d33595749de2c6e6bb8d
Gabe <wei.g.chu@gmail.com>
2020-03-25 16:25:57 +0800

    add 3rd exp

--- experiments/python_crf.py
+++ experiments/python_crf.py
@@ -1,13 +1,14 @@
 from data_reader.conll_reader import get_conll_eval_data
+from data_reader.wnut_reader import get_wnut_eval_data
 from evaluation.model_evaluator import ModelEvaluator
 from recognisers.crf_recogniser import CrfRecogniser
 from tokeniser.detokeniser import space_join_detokensier
 from tokeniser.tokeniser import nltk_word_tokenizer
 
-from .manage_experiments import CRF_EXP, crf_1, crf_2
+from .manage_experiments import CRF_EXP, crf_1, crf_2, crf_3
 from .mlflow_log import log_evaluation_to_mlflow
 
-PARAMS = [crf_1, crf_2]
+PARAMS = [crf_1, crf_2, crf_3]
 RUN_NAME = "crf_no_pos"
 
 for param in PARAMS:
@@ -26,11 +27,29 @@ for param in PARAMS:
         model_path=param["model_path"],
         tokenizer=param["tokeniser"],
     )
-    evaluator = ModelEvaluator(recogniser, ["I-PER"], nltk_word_tokenizer)
-    X_test, y_test = get_conll_eval_data(
-        file_path=param["eval_data"], detokenizer=space_join_detokensier
-    )
 
-    log_evaluation_to_mlflow(
-        CRF_EXP, param, recogniser, evaluator, X_test, y_test, run_name=RUN_NAME
-    )
+    evaluator = None
+    X_test = None
+    y_test = None
+
+    if "conll2003" in param["eval_data"]:
+        X_test, y_test = get_conll_eval_data(
+            file_path=param["eval_data"], detokenizer=space_join_detokensier
+        )
+        evaluator = ModelEvaluator(recogniser, ["I-PER"], nltk_word_tokenizer)
+
+    elif "wnut2017" in param["eval_data"]:
+        X_test, y_test = get_wnut_eval_data(
+            file_path=param["eval_data"], detokenizer=space_join_detokensier
+        )
+        evaluator = ModelEvaluator(
+            recogniser,
+            ["I-PER"],
+            nltk_word_tokenizer,
+            to_eval_labels={"I-PER": "I-person"},
+        )
+
+    if evaluator and X_test and y_test:
+        log_evaluation_to_mlflow(
+            CRF_EXP, param, recogniser, evaluator, X_test, y_test, run_name=RUN_NAME
+        )
