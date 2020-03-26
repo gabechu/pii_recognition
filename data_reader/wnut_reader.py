91e0efa6a52c7f8c4ad84e4120213df314452fda
Gabe <wei.g.chu@gmail.com>
2020-03-26 11:44:29 +0800

    add test for wnut reader

--- data_reader/wnut_reader.py
+++ data_reader/wnut_reader.py
@@ -23,13 +23,19 @@ def get_wnut_eval_data(
     with open(file_path, "r") as f:
         lines = f.readlines()
         for line in lines:
-            try:
-                token, entity_tag = line.split()
+            data = line.split()
+            if data:
+                token, entity_tag = data
                 sentence_tokens.append(token)
                 sentence_entities.append(entity_tag)
-            except ValueError:  # hit end of sentence
+            else:  # hit empty line
                 sents.append(detokenizer(sentence_tokens))
                 labels.append(map_bio_to_io_labels(sentence_entities))
                 sentence_tokens = []
                 sentence_entities = []
+
+    # clear up
+    if sentence_tokens and sentence_entities:
+        sents.append(detokenizer(sentence_tokens))
+        labels.append(map_bio_to_io_labels(sentence_entities))
     return sents, labels
