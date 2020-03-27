4cc3fde0df7549d80928728fcc613632302cbb97
Gabe <wei.g.chu@gmail.com>
2020-03-27 20:47:39 +0800

    black format

--- recognisers/first_letter_uppercase_recogniser.py
+++ recognisers/first_letter_uppercase_recogniser.py
@@ -26,9 +26,7 @@ class FirstLetterUppercaseRecogniser(EntityRecogniser):
 
     def analyse(self, text: str, entities: List[str]) -> List[SpanLabel]:
         tokens = self.tokeniser(text)
-        entity_tags = [
-            self.PER if token.text.istitle() else "O" for token in tokens
-        ]
+        entity_tags = [self.PER if token.text.istitle() else "O" for token in tokens]
 
         spans = token_labels_to_span_labels(tokens, entity_tags)
         return [span for span in spans if span.entity_type in entities]
