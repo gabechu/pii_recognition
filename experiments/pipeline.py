5cf8f62e3f92fd29cac16a25d77428a27f99d39c
Gabe <wei.g.chu@gmail.com>
2020-03-30 14:45:03 +0800

    pipeline skelton

---
+++ experiments/pipeline.py
@@ -0,0 +1,28 @@
+from dagster import solid
+
+
+@solid
+def register_recognisers():
+    ...
+
+
+@solid
+def get_recogniser():
+    ...
+
+
+@solid
+def initialise_recogniser():
+    ...
+
+
+@solid
+def get_evaluation_data():
+    ...
+
+
+@solid
+def initialise_evaluator():
+    ...
+
+
