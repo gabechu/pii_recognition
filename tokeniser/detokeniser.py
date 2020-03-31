9269fc2fd3263f0b4c232513a9951c270b9b9ff0
Gabe <wei.g.chu@gmail.com>
2020-03-31 10:40:05 +0800

    add tokeniser registry

--- tokeniser/detokeniser.py
+++ tokeniser/detokeniser.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Callable
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 
@@ -8,3 +8,16 @@ def space_join_detokensier(tokens: List[str]) -> str:
 
 def treebank_detokeniser(tokens: List[str]) -> str:
     return TreebankWordDetokenizer().detokenize(tokens)
+
+
+class DetokeniserRegistry:
+    def __init__(self):
+        self.registry = {}
+        self.add_predefined_detokeniser()
+
+    def add_predefined_detokeniser(self):
+        self.add_detokeniser(space_join_detokensier)
+        self.add_detokeniser(treebank_detokeniser)
+
+    def add_detokeniser(self, detokeniser: Callable):
+        self.registry[detokeniser.__name__] = detokeniser
