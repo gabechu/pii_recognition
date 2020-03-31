cce64d2ab3dfe859cf7f4ab382863d123b3076ec
Gabe <wei.g.chu@gmail.com>
2020-03-31 08:52:17 +0800

    complete recogniser init solid

--- experiments/pipeline.py
+++ experiments/pipeline.py
@@ -1,38 +1,39 @@
-from dagster import execute_pipeline, pipeline, solid
+import inspect
 
-from recognisers.crf_recogniser import CrfRecogniser
-from recognisers.first_letter_uppercase_recogniser import FirstLetterUppercaseRecogniser
-from recognisers.flair_recogniser import FlairRecogniser
+from dagster import Field, List, String, execute_pipeline, pipeline, solid
+
 from recognisers.recogniser_registry import RecogniserRegistry
-from recognisers.spacy_recogniser import SpacyRecogniser
-from recognisers.stanza_recogniser import StanzaRecogniser
 
 from .types import RecogniserRegistryDT
 
 
 @solid
-def register_recognisers(context) -> RecogniserRegistryDT:
+def get_recogniser(context, recogniser_name: str):
     registry = RecogniserRegistry()
-
-    registry.add_recogniser(CrfRecogniser)
-    registry.add_recogniser(FirstLetterUppercaseRecogniser)
-    registry.add_recogniser(FlairRecogniser)
-    registry.add_recogniser(SpacyRecogniser)
-    registry.add_recogniser(StanzaRecogniser)
-
-    return registry
+    return registry.get_recogniser(recogniser_name)
 
 
-@solid
-def get_recogniser(
-    context, recogniser_registry: RecogniserRegistryDT, recogniser_name: str
-):
-    return recogniser_registry.registry[recogniser_name]
+@solid(
+    config={
+        "supported_entities": Field(list, is_required=True),
+        "supported_languages": Field(list, is_required=True),
+        "model_path": Field(String, is_required=False),
+        "tokeniser": Field(
+            String, is_required=False, default_value="nltk_word_tokenizer"
+        ),
+        "model_name": Field(String, is_required=False),
+    }
+)
+def initialise_recogniser(context, recogniser_cls):
+    required_args = inspect.getfullargspec(recogniser_cls).args
+    required_args.pop(0)  # remove self
 
-
-@solid
-def initialise_recogniser(context):
-    ...
+    filtered_config = {
+        key: value
+        for key, value in context.solid_config.items()
+        if key in required_args
+    }
+    return recogniser_cls(**filtered_config)
 
 
 @solid
@@ -52,15 +53,22 @@ def evaluate_and_logging(context):
 
 @pipeline
 def evaluation_pipeline():
-    return get_recogniser(register_recognisers())
+    return initialise_recogniser(get_recogniser())
 
 
 if __name__ == "__main__":
     environment_dict = {
         "solids": {
             "get_recogniser": {
-                "inputs": {"recogniser_name": {"value": "CrfRecogniser"}}
-            }
+                "inputs": {"recogniser_name": {"value": "SpacyRecogniser"}}
+            },
+            "initialise_recogniser": {
+                "config": {
+                    "supported_entities": ["LOC", "MISC", "ORG", "PER"],
+                    "supported_languages": ["en", "de", "es", "fr", "it", "pt", "ru"],
+                    "model_name": "xx_ent_wiki_sm",
+                }
+            },
         }
     }
 
--- recognisers/recogniser_registry.py
+++ recognisers/recogniser_registry.py
@@ -1,11 +1,24 @@
 from typing import Type
 
+from .crf_recogniser import CrfRecogniser
 from .entity_recogniser import EntityRecogniser
+from .first_letter_uppercase_recogniser import FirstLetterUppercaseRecogniser
+from .flair_recogniser import FlairRecogniser
+from .spacy_recogniser import SpacyRecogniser
+from .stanza_recogniser import StanzaRecogniser
 
 
 class RecogniserRegistry:
     def __init__(self):
         self.registry = {}
+        self.add_predefined_recognisers()
+
+    def add_predefined_recognisers(self):
+        self.add_recogniser(CrfRecogniser)
+        self.add_recogniser(FirstLetterUppercaseRecogniser)
+        self.add_recogniser(FlairRecogniser)
+        self.add_recogniser(SpacyRecogniser)
+        self.add_recogniser(StanzaRecogniser)
 
     def add_recogniser(self, recogniser: Type[EntityRecogniser]):
         # TODO: myerror
