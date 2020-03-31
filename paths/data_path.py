68f367abc9e10ddf70b80a82464ed42778126e0a
Gabe <wei.g.chu@gmail.com>
2020-03-31 13:02:17 +0800

    add path class

---
+++ paths/data_path.py
@@ -0,0 +1,29 @@
+import re
+from typing import Match, Type
+
+
+class Path:
+    _pattern_str = None
+
+    def __init__(self, path: str):
+        self.path = path
+        self._pattern_to_attrs(self.get_pattern())
+
+    def get_pattern(self) -> Match:
+        if not self._pattern_str:
+            raise AttributeError("Pattern has not been defined.")
+
+        return re.match(self._pattern_str, self.path)
+
+    def _pattern_to_attrs(self, pattern: Match):
+        for key, value in pattern.groupdict().items():
+            setattr(self, key, value)
+
+
+def create_path_subclass(cls_name: str, pattern: str) -> Type[Path]:
+    return type(cls_name, (Path,), {"_pattern": pattern})
+
+
+DataPath = create_path_subclass(
+    "DataPath", "datasets/(?P<data_name>[a-zA-Z]+)(?P<version>\d+)/"
+)
