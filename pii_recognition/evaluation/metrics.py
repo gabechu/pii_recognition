a9c339205f05032ba4a01f094ebd632f60f89516
Gabe Chu <60019811+gabechu@users.noreply.github.com>
2020-09-22 15:17:32 +0800

    Add calculations of precision and recall on a particular label (#55)
    * add precision and recall metrics
    
    * add strategy for character based evaluation
    
    * test label encoding
    
    * update rules and edge case
    
    * split test
    
    * remove files will push in another pr
    
    * use black formatter for consistentcy
    
    * fix flake8

--- pii_recognition/evaluation/metrics.py
+++ pii_recognition/evaluation/metrics.py
@@ -1,4 +1,7 @@
+from typing import List, Union
+
 import numpy as np
+from sklearn.metrics import precision_score, recall_score
 
 
 def compute_f_beta(precision: float, recall: float, beta: float = 1.0) -> float:
@@ -6,3 +9,25 @@ def compute_f_beta(precision: float, recall: float, beta: float = 1.0) -> float:
         return np.nan
 
     return ((1 + beta ** 2) * precision * recall) / (((beta ** 2) * precision) + recall)
+
+
+def compute_label_precision(
+    y_true: List, y_pred: List, label_name: Union[int, str]
+) -> float:
+    """Compute recall for a designated label.
+
+    This can calculate precision for a particular label for both binary and
+    multi-class settings.
+    """
+    return precision_score(y_true, y_pred, average=None, labels=[label_name])[0]
+
+
+def compute_label_recall(
+    y_true: List, y_pred: List, label_name: Union[int, str]
+) -> float:
+    """Compute recall for a designated label.
+
+    This can calculate recall for a particular label for both binary and
+    multi-class settings.
+    """
+    return recall_score(y_true, y_pred, average=None, labels=[label_name])[0]
--- pii_recognition/evaluation/metrics_test.py
+++ pii_recognition/evaluation/metrics_test.py
@@ -1,8 +1,11 @@
 import numpy as np
-from .metrics import compute_f_beta
+from numpy.testing import assert_almost_equal
+
+from .metrics import compute_f_beta, compute_label_precision, compute_label_recall
 
 
 def test_compute_f_beta():
+    # TODO: break into separate test functions
     actual = compute_f_beta(0.0, 0.0)
     assert np.isnan(actual)
 
@@ -20,3 +23,85 @@ def test_compute_f_beta():
 
     actual = compute_f_beta(0.3, 0.5, beta=2.0)
     assert np.isclose(actual, 0.44117647058)
+
+
+def test_compute_label_precision_for_int_labels():
+    y_true = [0, 1]
+    y_pred = [1, 1]
+    actual = compute_label_precision(y_true, y_pred, label_name=1)
+    assert actual == 0.5
+
+
+def test_compute_label_precision_for_str_labels():
+    y_true = ["label_0", "label_1"]
+    y_pred = ["label_1", "label_1"]
+    actual = compute_label_precision(y_true, y_pred, label_name="label_1")
+    assert actual == 0.5
+
+
+def test_compute_label_precision_for_binary():
+    y_true = [0, 0, 1]
+    y_pred = [0, 1, 1]
+    actual = compute_label_precision(y_true, y_pred, label_name=1)
+    assert actual == 0.5
+
+
+def test_compute_label_precision_for_multiclass():
+    y_true = [0, 0, 0, 1, 0, 0, 2, 0, 2, "label_0", 0, "label_0", "label_0"]
+    y_pred = [0, 0, 0, 1, 1, 1, 2, 2, 2, "label_0", "label_0", "label_0", "label_0"]
+    actual = compute_label_precision(y_true, y_pred, label_name=1)
+    assert_almost_equal(actual, 0.3333333)
+    actual = compute_label_precision(y_true, y_pred, label_name=2)
+    assert_almost_equal(actual, 0.6666666)
+    actual = compute_label_precision(y_true, y_pred, label_name="label_0")
+    assert actual == 0.75
+
+
+def test_compute_label_precision_for_nonexist_label():
+    # nonexist label is a label not in presence in either true or predicted
+    # results
+    y_true = [1]
+    y_pred = [1]
+    actual = compute_label_precision(y_true, y_pred, label_name=2)
+    assert actual == 0.0
+
+
+def test_compute_label_recall_for_int_labels():
+    y_true = [1]
+    y_pred = [1]
+    actual = compute_label_recall(y_true, y_pred, label_name=1)
+    assert actual == 1.0
+
+
+def test_compute_label_recall_for_str_labels():
+    y_true = ["label_0", "label_1"]
+    y_pred = ["label_0", "label_0"]
+    actual = compute_label_recall(y_true, y_pred, label_name=1)
+    assert actual == 0.0
+
+
+def test_compute_label_recall_for_binary():
+    y_true = [0, 1, 1]
+    y_pred = [0, 0, 1]
+    actual = compute_label_recall(y_true, y_pred, label_name=1)
+    assert actual == 0.5
+
+
+def test_compute_label_recall_for_multiclass():
+    y_true = [0, 0, 0, 1, 1, 1, 2, 2, 2, "label_0", "label_0", "label_0", "label_0"]
+    y_pred = [0, 0, 0, 1, 0, 0, 2, 0, 2, "label_0", 0, "label_0", "label_0"]
+    actual = compute_label_recall(y_true, y_pred, label_name=1)
+    assert_almost_equal(actual, 0.3333333)
+    actual = compute_label_recall(y_true, y_pred, label_name=2)
+    assert_almost_equal(actual, 0.6666666)
+    actual = compute_label_recall(y_true, y_pred, label_name="label_0")
+    assert actual == 0.75
+
+
+def test_compute_label_recall_for_nonexist_label():
+    # nonexist label is a label not in presence in either true or predicted
+    # results
+    y_true = [1]
+    y_pred = [1]
+    actual = compute_label_recall(y_true, y_pred, label_name=2)
+    assert actual == 0.0
