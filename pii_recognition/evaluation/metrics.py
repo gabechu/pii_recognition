1c1f0e1ecec67befab44e8c23d047eb823263759
Gabe Chu <60019811+gabechu@users.noreply.github.com>
2020-09-24 12:12:29 +0800

    Fix tests on metrics (#56)
    * split test and add test for edge cases
    
    * add comment about ValueError
    
    * arguments of precison and recall func must be str
    
    * add test
    
    * use typevar

--- pii_recognition/evaluation/metrics.py
+++ pii_recognition/evaluation/metrics.py
@@ -1,7 +1,10 @@
-from typing import List, Union
+from typing import List, TypeVar
 
 import numpy as np
 from sklearn.metrics import precision_score, recall_score
+
+# LT for label type
+LT = TypeVar("LT", int, str)
 
 
 def compute_f_beta(precision: float, recall: float, beta: float = 1.0) -> float:
@@ -12,22 +15,22 @@ def compute_f_beta(precision: float, recall: float, beta: float = 1.0) -> float:
 
 
 def compute_label_precision(
-    y_true: List, y_pred: List, label_name: Union[int, str]
+    y_true: List[LT], y_pred: List[LT], label_name: LT,
 ) -> float:
     """Compute recall for a designated label.
 
-    This can calculate precision for a particular label for both binary and
-    multi-class settings.
+    This can calculate precision of a particular label for both binary and multi-class
+    settings. The invoked sklearn function is not stable on string and integer mixed
+    labels, may encouter ValueError. So mixed types in an argument is not allowed.
     """
     return precision_score(y_true, y_pred, average=None, labels=[label_name])[0]
 
 
-def compute_label_recall(
-    y_true: List, y_pred: List, label_name: Union[int, str]
-) -> float:
+def compute_label_recall(y_true: List[LT], y_pred: List[LT], label_name: LT,) -> float:
     """Compute recall for a designated label.
 
-    This can calculate recall for a particular label for both binary and
-    multi-class settings.
+    This can calculate recall of a particular label for both binary and multi-class
+    settings. The invoked sklearn function is not stable on string and integer mixed
+    labels, may encouter ValueError. So mixed types in an arguments is not allowed.
     """
     return recall_score(y_true, y_pred, average=None, labels=[label_name])[0]
