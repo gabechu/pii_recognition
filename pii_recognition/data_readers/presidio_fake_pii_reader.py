b6bc9950baa26ce7f97406a1f98e2948f2ba509d
Gabe <wei.g.chu@gmail.com>
2020-10-08 10:15:38 +0800

    update reader to use DataItem

--- pii_recognition/data_readers/presidio_fake_pii_reader.py
+++ pii_recognition/data_readers/presidio_fake_pii_reader.py
@@ -1,7 +1,20 @@
 from typing import Dict, List, Set
+from typing_extensions import TypedDict
 
-from pii_recognition.data_readers.data import Data
+from pii_recognition.data_readers.data import Data, DataItem, Entity
 from pii_recognition.utils import load_json_file
+
+# mypy assumes that Dict is homogeneous, however, we
+# need it to be heterogenous thus use TypedDict
+PresidioSpan = TypedDict(
+    "PresidioSpan",
+    {
+        "entity_type": str,
+        "entity_value": str,
+        "start_position": int,
+        "end_position": int,
+    },
+)
 
 
 class PresidioFakePiiReader:
@@ -13,13 +26,18 @@ class PresidioFakePiiReader:
                 unique_labels.add(span["entity_type"])
         return unique_labels
 
+    def _span_to_entity(self, span: PresidioSpan) -> Entity:
+        return Entity(span["entity_type"], span["start_position"], span["end_position"])
+
     def build_data(self, file_path: str) -> Data:
         data = load_json_file(file_path)
         supported_entities = self._get_supported_entities(data)
 
+        items = []
+        for item in data:
+            entities = [self._span_to_entity(span) for span in item["spans"]]
+            items.append(DataItem(text=item["full_text"], true_labels=entities))
+
         return Data(
-            texts=[item["full_text"] for item in data],
-            labels=[item["spans"] for item in data],
-            supported_entities=supported_entities,
-            is_io_schema=False,
+            items=items, supported_entities=supported_entities, is_io_schema=False,
         )
