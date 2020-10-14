4fd79eb231e75add230baca0e78dec2fcdd5a6d6
Gabe <wei.g.chu@gmail.com>
2020-10-14 19:48:35 +0800

    fix empty precisoins and recalls

--- pii_recognition/pipelines/pii_validation_pipeline.py
+++ pii_recognition/pipelines/pii_validation_pipeline.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Optional, Set
 
-from pakkr import returns
+from pakkr import Pipeline, returns
+from pii_recognition.constants import ROOT_DIR
 from pii_recognition.data_readers.data import Data
 from pii_recognition.data_readers.presidio_fake_pii_reader import PresidioFakePiiReader
 from pii_recognition.evaluation.character_level_evaluation import (
@@ -12,13 +13,17 @@ from pii_recognition.evaluation.character_level_evaluation import (
 )
 from pii_recognition.recognisers import registry as recogniser_registry
 from pii_recognition.recognisers.entity_recogniser import EntityRecogniser
-from pii_recognition.utils import dump_to_json_file
+from pii_recognition.utils import dump_to_json_file, load_yaml_file
 
 
 @returns(Data)
 def read_benchmark_data(benchmark_data_file: str) -> Data:
     reader = PresidioFakePiiReader()
-    return reader.build_data(benchmark_data_file)
+    data = reader.build_data(benchmark_data_file)
+
+    # remove empty items
+    data.items = list(filter(lambda item: item.text != "", data.items))
+    return data
 
 
 @returns(Data)
@@ -28,7 +33,7 @@ def identify_pii_entities(
     recogniser: EntityRecogniser = recogniser_registry.create_instance(
         recogniser_name, recogniser_params
     )
-
+    data.items = data.items[:10]
     for item in data.items:
         item.pred_labels = recogniser.analyse(item.text, recogniser.supported_entities)
     return data
@@ -57,7 +62,6 @@ def calculate_precisions_and_recalls(
         )
         ticket_score = TextScore(precisions=ent_precisions, recalls=ent_recalls)
         scores.append(ticket_score)
-
     return scores
 
 
@@ -76,11 +80,12 @@ def calculate_aggregate_metrics(
     return results
 
 
-@returns
+@returns()
 def report_results(results: Dict[str, float], dump_file: str):
     dump_to_json_file(results, dump_file)
 
 
+# TODO: change f1 to fbeta
 def get_rollup_f1s_on_pii(
     scores: List[TextScore], f1_beta: float, recall_threshold: Optional[float]
 ) -> List[float]:
@@ -88,6 +93,41 @@ def get_rollup_f1s_on_pii(
     for text_score in scores:
         precisions = [p.precision for p in text_score.precisions]
         recalls = [r.recall for r in text_score.recalls]
-        f1 = compute_pii_detection_f1(precisions, recalls, recall_threshold, f1_beta)
+
+        if not precisions and recalls:
+            # there are true entities but the system predicts nothing
+            f1 = 0.0
+        elif precisions and not recalls:
+            # there is no true entity but the system predicts something
+            f1 = 0.0
+        elif not precisions and not recalls:
+            # there is no true entity and the system predicts nothing
+            f1 = 1.0
+        else:
+            f1 = compute_pii_detection_f1(
+                precisions, recalls, recall_threshold, f1_beta
+            )
         f1s.append(f1)
     return f1s
+
+
+def exec_pipeline(config_yaml):
+    pipeline = Pipeline(
+        read_benchmark_data,
+        identify_pii_entities,
+        calculate_precisions_and_recalls,
+        calculate_aggregate_metrics,
+        report_results,
+        name="pii_validation_pipeline",
+    )
+
+    config = load_yaml_file(config_yaml)
+    if config:
+        # conversions to meet requirements on type checks
+        config["grouped_targeted_labels"] = [
+            set(item) for item in config["grouped_targeted_labels"]
+        ]
+        config["nontargeted_labels"] = set(config["nontargeted_labels"])
+        return pipeline(**config)
+    else:
+        raise ValueError("Config YAML is empty.")
